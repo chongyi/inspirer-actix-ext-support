b37e2b40d26c0efad8a473cec524b932cab5604e
Chongyi <xpz3847878@163.com>
2021-05-14 18:01:25 +0800

    增加数据访问对象 trait

--- inspirer-actix-modules/database-sqlx/src/lib.rs
+++ inspirer-actix-modules/database-sqlx/src/lib.rs
@@ -1,10 +1,31 @@
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate async_trait;
 
 pub mod config;
 mod module_provider;
 
 pub use module_provider::mysql;
+
+use sqlx::{Executor, Database};
+
+/// 数据访问对象查询器
+///
+/// 实现该 Trait 的对象都可以作为查询器被调度
+#[async_trait]
+pub trait DAO<D: Database> {
+    type Result;
+
+    async fn run<'a, E>(&self, executor: E) -> Self::Result
+        where E: Executor<'a, Database=D>
+    ;
+}
+
+pub mod prelude {
+    pub use sqlx;
+    pub use crate::DAO;
+}
 
 #[cfg(test)]
 mod tests {
