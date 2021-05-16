85216c82b1aa914a28970ebc0e1456a6cccc8b65
chongyi <xpz3847878@163.com>
2021-05-16 18:22:00 +0800

    扩展支持

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+pub use inspirer_actix_ext_core::preludes::{ModuleProvider, ModuleContainer, ModuleFactoryFn};
+
+#[cfg(feature = "database")]
+pub mod database {
+    pub use inspirer_actix_module_database_sqlx::prelude::*;
+}
+
+#[cfg(feature = "redis")]
+pub mod redis {
+    pub use inspirer_actix_module_redis::prelude::*;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
