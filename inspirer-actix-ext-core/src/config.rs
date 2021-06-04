b1ba389bf78158ef21b773b94c9c3b5cfbfc18f8
Chongyi <xpz3847878@163.com>
2021-06-04 15:45:47 +0800

    增加配置模块加载时的日志，以及错误处理

--- inspirer-actix-ext-core/src/config.rs
+++ inspirer-actix-ext-core/src/config.rs
@@ -7,12 +7,18 @@ use std::pin::Pin;
 pub fn config_provider(config_files: Vec<String>) -> impl Fn(&ModuleProvider) -> Pin<Box<dyn Future<Output=Result<Config, ConfigError>>>>
 {
     move |module_provider| {
+        debug!("Register configuration manager module.");
         let config_files = config_files.clone();
         Box::pin(async move {
             let mut config = Config::new();
 
             for config_file in config_files.iter() {
-                config.merge(File::from(Path::new(config_file)));
+                debug!("Load config file: {}", config_file);
+                config.merge(File::from(Path::new(config_file)))
+                    .map_err(|err| {
+                        error!("Load config file error: {}", err);
+                        err
+                    })?;
             }
 
             Ok(config)
