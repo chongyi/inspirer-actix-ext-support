4136d26f67fe4673aa5a7a4f2917a186c0760957
chongyi <xpz3847878@163.com>
2021-06-13 02:10:27 +0800

    增加 DAO 细分 trait

--- inspirer-actix-modules/database-sqlx/src/lib.rs
+++ inspirer-actix-modules/database-sqlx/src/lib.rs
@@ -3,36 +3,16 @@ extern crate log;
 #[macro_use]
 extern crate async_trait;
 
+use sqlx::{Database, Executor};
+
 pub mod config;
-mod module_provider;
-
-pub use module_provider::mysql;
-
-use sqlx::{Executor, Database};
-
-/// 数据访问对象查询器
-///
-/// 实现该 Trait 的对象都可以作为查询器被调度
-#[async_trait]
-pub trait DAO<D: Database> {
-    type Result;
-
-    async fn run<'a, E>(&self, executor: E) -> Self::Result
-        where E: Executor<'a, Database=D>
-    ;
-}
+pub mod module_provider;
+pub mod dao;
 
 pub mod prelude {
     pub use sqlx;
-    pub use crate::DAO;
-    pub use crate::config;
-    pub use crate::module_provider::mysql;
-}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    pub use crate::config;
+    pub use crate::dao::DAO;
+    pub use crate::module_provider::mysql;
 }
